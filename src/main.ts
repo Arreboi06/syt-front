67acb227fcf6cb7954c910dc47117338eec02135
Arreboi06 <3562419275@qq.com>
2025-04-23 22:00:35 +0800

    项目初始化完成

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+//vue3框架提供的方法createApp方法,可以用来创建应用实例方法
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// 引入根组件App
+import App from '@/App.vue'
+////利用createApp方法创建应用实例,且将应用实例挂载到挂载点上
+const app = createApp(App);
+//挂载
+app.mount('#app');
